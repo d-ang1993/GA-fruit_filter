530512603a2f9a08091042d4c1085e549aa68a90
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-03 19:20:06 -0400

    [GA] fruit filter example

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FruitContainer from './FruitContainer';
+
+const fruitList = [
+  'Acai',
+  'Aceola',
+  'Apple',
+  'Apricots',
+  'Avocado',
+  'Banana',
+  'Blackberry',
+  'Blueberries',
+  'Camu Camu berry',
+  'Cherries',
+  'Coconut',
+  'Cranberry',
+  'Cucumber',
+  'Currents',
+  'Dates',
+  'Durian',
+  'Fig',
+  'Goji berries',
+  'Gooseberry',
+  'Grapefruit',
+  'Grapes',
+  'Jackfruit',
+  'Kiwi',
+  'Kumquat',
+  'Lemon',
+  'Lime',
+  'Lucuma',
+  'Lychee',
+  'Mango',
+  'Mangosteen',
+  'Melon',
+  'Mulberry',
+  'Nectarine',
+  'Orange',
+  'Papaya',
+  'Passion Fruit',
+  'Peach',
+  'Pear',
+  'Pineapple',
+  'Plum',
+  'Pomegranate',
+  'Pomelo',
+  'Prickly Pear',
+  'Prunes',
+  'Raspberries',
+  'Strawberries',
+  'Tangerine/Clementine',
+  'Watermelon'
+]
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <FruitContainer fruits = {fruitList}/>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/FruitContainer.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+
+const FruitList = props => (
+  <ul>
+     {props.fruits.map(fruit => <li>{fruit}</li>)}
+  </ul>
+)
+
+const FruitFilter = props => (
+  <div>
+    <label htmlFor="fruit-filter">Filter these Fruits: </label>
+    <input type="text" value={props.value} onChange={props.onChange} name="fruit-filter" />
+   </div>
+)
+
+
+class FruitContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      fruitsToDisplay: props.fruits,
+      unmatchedFruits: [],
+      filterValue: ''
+    }
+
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+  }
+
+  handleFilterChange(event) {
+    event.preventDefault()
+    const filterValue = event.target.value;
+    this.setState((prevState, props) => {
+      // remove fruits that don't contain the filter value
+      const filteredFruitList = props.fruits.filter(fruit =>
+        fruit.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase()));
+      const unmatchedFruits = props.fruits.filter(fruit =>
+        !fruit.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase()));
+      // return new state with the filtered fruit list and the new value of the filter
+      return {
+        fruitsToDisplay: filteredFruitList,
+        unmatchedFruits: unmatchedFruits,
+        filterValue
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <FruitFilter value={this.state.filterValue} onChange={this.handleFilterChange} />
+        <h3> Matching </h3>
+        <FruitList fruits={this.state.fruitsToDisplay} />
+        <h3> Not-Matching </h3>
+        <FruitList fruits={this.state.unmatchedFruits} />
+      </div>
+    )
+  }
+
+}
+
+export default FruitContainer
